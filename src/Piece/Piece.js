4368f1c4ab429399d71814a34204221c2f93b153
mariastlouis <msantray@yahoo.com>
2018-03-31 10:51:58 -0600

    Piece component created

---
+++ src/Piece/Piece.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Piece = () => {
+  return (
+    <div className = "Piece">
+      <p> Piece component </p>
+    </div>
+  );
+};
+
+export default Piece;
