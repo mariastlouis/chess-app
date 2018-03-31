867e92c610d0dbad5c262cbf9dfd7548b342f1e1
mariastlouis <msantray@yahoo.com>
2018-03-31 10:53:59 -0600

    Square component created

---
+++ src/Square/Square.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Square = () => {
+  return (
+    <div className = "Square">
+      <p> Square component </p>
+    </div>
+  );
+};
+
+export default Square;
