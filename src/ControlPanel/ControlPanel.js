dc99cd38a5c73aeaf45bedc81b2a5f495690475e
mariastlouis <msantray@yahoo.com>
2018-04-01 21:18:34 -0600

    control panel component created

---
+++ src/ControlPanel/ControlPanel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import './ControlPanel.css'
+
+
+const ControlPanel = ({message}) => {
+  return (
+    <div className = "ControlPanel">
+     <h1> Control panel </h1>
+    </div>
+  );
+};
+
+export default ControlPanel;
