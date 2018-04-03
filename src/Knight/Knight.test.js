f1f0ec412b7c61666f861f89394a259093267620
mariastlouis <msantray@yahoo.com>
2018-04-03 13:18:52 -0600

    knight component tested with conditional rendering

--- src/Knight/Knight.test.js
+++ src/Knight/Knight.test.js
@@ -1,4 +1,51 @@
 import React from 'react';
-import Card from './Card.js';
+import Knight from './Knight.js';
 import {shallow} from 'enzyme';
+import * as enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
 
+let renderedPiece;
+
+describe('Knight', () =>{
+  beforeEach(() => {
+    renderedPiece = shallow(
+      <Knight 
+        mode = 'standard'
+      />);
+  });
+
+  it('should render corrrectly', () => {
+    expect(renderedPiece).toBeDefined();
+  });
+
+   it('should match the snapshot', () =>{
+    expect(renderedPiece).toMatchSnapshot();
+  });
+
+   it('should render a knight image by default', () => {
+     expect(renderedPiece.containsMatchingElement(
+      <img src="knight.png" className="knight gamepiece" alt="knight" />))
+      .toEqual(true);
+   })
+
+   it('should render a donkey if the mode if is on democrat mode', () => {
+    const renderedDemPiece = shallow (
+      <Knight mode = 'democrat' />)
+    expect(renderedDemPiece.containsMatchingElement(
+      <img src="demDonkey.png" className="knight gamepiece" alt="knight" />))
+      .toEqual(true);
+   });
+
+     it('should render an elephant if the mode if is on republican mode', () => {
+    const renderedRepPiece = shallow (
+      <Knight mode = 'republican' />)
+    console.log(renderedRepPiece.debug())
+    expect(renderedRepPiece.containsMatchingElement(
+     <img src="repElephant.png" className="knight gamepiece" alt="knight" />))
+      .toEqual(true);
+   });
+
+
+
+});
